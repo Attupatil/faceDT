a78ab2ea4bd568597b34575067ddaa3cb20f7e71
Atharva Patil <39415890+Attupatil@users.noreply.github.com>
2021-10-18 18:17:06 +0530

    Add files via upload

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+  return (
+    <div>
+      <p className='f3'>
+        {'FaceDT will detect faces in your pictures.Try it.'}
+      </p>
+      <p className='f6'>
+            {'Note: Try to use a .jpg link'}
+            </p>
+      <div className='center'>
+        <div className='form center pa4 br3 shadow-5'>
+          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+          <button
+            className='w-30 grow f4 link ph3 pv2 dib white bg-light-yellow'
+            onClick={onButtonSubmit}
+          >Detect</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ImageLinkForm;
